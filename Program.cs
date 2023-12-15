a98697027542d6fb7f7a999d70895c261b729dbe
Diogo-meneghini <54080093+Diogo-meneghini@users.noreply.github.com>
2023-12-14 21:01:45 -0300

    Desafio completo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Iphone iphone = new Iphone("123", "Iphone 12", "123d34a", 128);
+        Nokia nokia = new Nokia("1453", "nokia 12", "123d34a", 128);
+
+        nokia.Ligar();
+        iphone.Ligar();
+        Console.WriteLine($"{iphone.Memoria} {nokia.Memoria} {iphone.Imei}");
+    }
+}
